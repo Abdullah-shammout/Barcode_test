85ed5c83fc000f84fbfcd12c93b3da0d589c8b66
Abdullah Shammout <abdullahshammout1@gmail.com>
2025-10-16 09:26:40 +0300

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Barcode.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Barcode
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<BarcodeTypes> BarcodeType { get; set; }
+    }
+}
