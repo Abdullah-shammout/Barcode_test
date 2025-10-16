2dad2ed7056971bb2f97cc65402bed544c2c7dfe
Abdullah Shammout <abdullahshammout1@gmail.com>
2025-10-16 09:29:27 +0300

    Add files via upload

---
+++ Controllers/BarcodeController.cs
@@ -0,0 +1,24 @@
+﻿using Barcode;
+using Barcode.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Text;
+
+namespace AspMvcControllers.Controllers
+{
+
+    public class BarcodeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public BarcodeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult BarcodeTypes()
+        {
+            List<BarcodeTypes> BarcodeList = new List<BarcodeTypes>();
+            BarcodeList = (from obj in _context.BarcodeType select obj).ToList();
+            return View("BarcodeTypes");
+        }
+    } 
+}
---
+++ Models/BarcodeTypes.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Barcode.Models
+{
+    public class BarcodeTypes
+    {
+        [Key]
+        public int BarcodeId { get; set; }
+        public string Barcode { get; set; }
+    }
+}
